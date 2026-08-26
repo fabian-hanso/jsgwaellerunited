1dca29653b41562f7352653efdb221a4650c5a88
Fabian Hanso <97611743+FabianHansoS2@users.noreply.github.com>
2026-08-26 21:18:25 +0200

    Navlinks

--- components/shared/Navbar.tsx
+++ components/shared/Navbar.tsx
@@ -200,6 +200,18 @@ export default function Navbar() {
                 >
                   Partner
                 </a>
+                <a
+                  href="#"
+                  className="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50"
+                >
+                  Mitglied werden
+                </a>
+                <a
+                  href="#"
+                  className="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50"
+                >
+                  Impressum
+                </a>
               </div>
               {/* Social Links hier! */}
             </div>
